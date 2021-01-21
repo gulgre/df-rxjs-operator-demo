5723537d15e4518a4c8203cb8a2cdb9625aa74aa
Daniel Foster <DFoster@tql.com>
2021-01-21 15:00:41 -0500

    added code section to examples to show syntax

--- src/app/services/operation.service.ts
+++ src/app/services/operation.service.ts
@@ -43,6 +43,7 @@ export class OperationService {
                 description: `Named after the tap you put inside a pipe that lets you draw water without changing the main water pipeline, this lets you take the data 
                 and perform an operation on it (logging, sending elsewhere), and the data that came in is the same as the data that goes out. Open the console log (F12), 
                 and notice that as the data is coming through and outputting on the screen, the console log is also getting the same data logged to it.`,
+                code: 'tap((data) => console.log(data))',
                 observable$: observableFactory(),
                 callback: this.tapOperation
             },
@@ -52,6 +53,7 @@ export class OperationService {
                 description: `Similar to javascript's "map" method, this takes data incoming from an Observable, and allows you to transform the data into something else.
                 This is useful for transforming numeric data into a string message that contains that, doing calculations on incoming data, or fitting the incoming data into a
                 class that another component/method is expecting.`,
+                code: 'map((data) => `The incoming data is ${data}, and twice that is ${data * 2}.`)',
                 observable$: observableFactory(),
                 callback: this.mapOperation
             },
@@ -60,6 +62,7 @@ export class OperationService {
                 shortDescription: `Filters out incoming data if it doesn't match a certain criteria.`,
                 description: `Let's be honest, not every bit of data is useful. This operation lets you cut out incoming data that doesn't fit the mold. 
                 This example filters out all odd values. `,
+                code: 'filter((data) => data % 2 === 0)',
                 observable$: observableFactory(),
                 callback: this.filterOperation
             },
@@ -68,6 +71,7 @@ export class OperationService {
                 shortDescription: `Accumulates everything emitted into an array.`,
                 description: `Sometimes you want to combine all the data that comes through, either in an array, or a sum. In this example, the "accumulator" is just an array,
                 and each item that comes in gets added to the array.`,
+                code: 'scan((acc: number[], data: number) => [...acc, data], [])',
                 observable$: observableFactory(),
                 callback: this.scanOperation
             },
@@ -83,6 +87,8 @@ export class OperationService {
                 description: `In development, you will often want to see what the data looks like at a certain stage, prior to manipulating. Combining tap and map operators
                 allows you to do this very thing. You can also use tap-map combinations to make side calls to a server, or update information elsewhere in the app before
                 transforming raw data into a formatted value for the user to see. Use the console log window (F12) to see what's getting logged, prior to the value being mapped for display`,
+                code: 'tap(data => console.log(About to double ${data}`)),\r\n' + 
+                    'map((data: number) => `The incoming data is ${data}, and twice that is ${data * 2}.`)',
                 observable$: observableFactory(),
                 callback: this.tapMapOperation
             },
@@ -91,6 +97,9 @@ export class OperationService {
                 shortDescription: 'filters out items before accumulating them',
                 description: `Filtering on its own has little value, but if you can collect all the filtered values into one item, the advantages of filter grows exponentially. This example
                 filters out all odd values, like in the basic example, and then combines those results into an array, like in the basic example.`,
+                code: 
+                    'filter((data) => data % 2 === 0),\r\n' + 
+                    'scan((acc: number[], data: number) => [...acc, data], [])',
                 observable$: observableFactory(),
                 callback: this.filterScanOperation
             },
